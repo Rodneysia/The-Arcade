e248cf0db89d4af1723c07d7c1a88a726176aefd
Rodneysia <rodneysia.musgraves@gmail.comgit config --global user.name Rodneysiagit config --global user.email rodneysia.musgraves@gmail.com>
2022-11-10 23:36:08 -0600

    Game functionality added

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const cells = document.querySelectorAll('.cell');
 const gameStatus = document.querySelector('.gameStatus'); 
 const restartBtn = document.getElementById('restartBtn');
 const winningConditions = [
+    //Rows
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
+    //Columns
     [0, 3, 6],
     [1, 4, 7],
     [2, 5, 8],
+    //Diagonals
     [0, 4, 8],
     [2, 4, 6]
   ];
@@ -25,8 +28,8 @@ buildInitialState();
 function buildInitialState () {
     cells.forEach(cell => cell.addEventListener("click", cellClick));
     restartBtn.addEventListener("click", restartBoard);
-    gameStatus.textContent = currentPlayer + " " + "'s turn";
-    console.log(gameStatus);
+    gameStatus.textContent = "Player " + currentPlayer + " " + "'s turn";
+    //console.log(gameStatus);
     gameIsActive = true;
 }
 
@@ -40,18 +43,55 @@ function cellClick () {
 }
 
 function updateCell (cell, index){
-    boardChoices[index] = currentPlayer;
+    boardChoices[index] = currentPlayer; //updates the cell with the current player
     cell.textContent = currentPlayer;
 }
-function changePlayer () {
-    currentPlayer = (currentPlayer == "X") ? "O" : "X";
-    gameStatus.textContent = currentPlayer + " " + "'s turn";
+function rotatePlayer () {
+    currentPlayer = (currentPlayer == "X") ? "O" : "X"; //rotate x's and o's
+    gameStatus.textContent = "Player " + currentPlayer + " " + "'s turn"; //updates the game status message section
 }
 
-function checkWinner() {
 
+function checkWinner() {
+   let gameOver = false;
+   
+   for (let i =0; i < winningConditions.length; i++){ //itterate through winning conditions
+    const condition = winningConditions[i];
+    const cellA = boardChoices[condition[0]]; //8 winning conditions & each winning condition has 3 indexes (O, 1 & 2)
+    const cellB = boardChoices[condition[1]];
+    const cellC = boardChoices[condition[2]];
+
+    if (cellA == "" || cellB == "" || cellC == ""){ //if each index is empty continue
+        continue;
+    }
+    if (cellA == cellB && cellB == cellC){ //if each index is the same value x or o, game over
+        gameOver = true;
+        break;
+    }
+   }
+    if(gameOver) {
+        gameStatus.textContent = "Player " + currentPlayer + " " + "is the winner! Press the Restart button to play again!";
+        gameIsActive = false;
+    }
+    else if (!boardChoices.includes("")){ //if no more empty cells & no winner game is a tie
+        gameStatus.textContent = "Tie Game! Press the Restart button to play again!";
+        gameIsActive = false;
+    }
+    else {
+        rotatePlayer();
+    }
 }
  
 function restartBoard () {
+    currentPlayer = "X"; //reset current player to x
+    boardChoices = [
+        "", "", "", 
+        "", "", "", 
+        "", "", ""
+    ];
+    cells.forEach(cell => cell.textContent = ""); //empty all cells
+    gameStatus.textContent = "Player " + currentPlayer + " " + "'s turn"; //game restarts with player x marking first
+    gameIsActive = true;
+
 
 }
